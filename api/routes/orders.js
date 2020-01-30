8eb9fa3258ab47aaf0904d898cf3aa6a177a49dc
phemidawhiz <feminiscence@gmail.com>
2020-01-30 14:23:11 +0100

    Setup server, routes and errors/cors handling

---
+++ api/routes/orders.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders were fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
+    res.status(201).json({
+        message: 'Order was created',
+        order: product
+    });
+});
+
+router.get('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order details',
+        orderId: req.params.orderId
+    });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order deleted',
+        orderId: req.params.orderId
+    });
+});
+
+router.patch('/:orderId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order updated!',
+        orderId: req.params.orderId
+    });
+});
+
+
+module.exports = router;
